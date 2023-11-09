e712a7413e22f6217074263d502879edd6449206
Xindi Deng <xd2287@columbia.edu>
2023-11-09 17:11:17 -0500

    update v2.2

--- LF2/lambda_function.py
+++ LF2/lambda_function.py
@@ -23,7 +23,7 @@ lex_client = boto3.client('lexv2-runtime')
 
 def lambda_handler(event, context):
     # TODO implement
-    print("Updated Lambda v2.1")
+    print("Updated Lambda v2.2")
     print("event is: ",event)
     # print("context is: ",context)
     
